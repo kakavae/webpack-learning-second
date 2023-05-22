d211f23d4c480d89da7b01c0121d0c9455d96612
KAKA <1442710830@qq.com>
2023-05-22 21:22:45 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -19,29 +19,6 @@ module.exports = {
   devServer: {
     /* 默认将dist目录作为静态资源的目录 */
     static: path.resolve(__dirname, './dist'),
-<<<<<<< HEAD
-    /* 设置是否在服务器端压缩代码--服务器向浏览器传输的时候文件是压缩的，加快速度，
-    响应头中就会有 Content-Encoding: gzip */
-    compress: true,
-    /* 配置端口号 */
-    port: 3000,
-    /* 配置响应的头部信息 */
-    headers: {
-      'X-Access-Token': 'abc'
-    },
-
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4001',
-        /* 路径重写 */
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  },
-=======
->>>>>>> fixed
 
     /* 开启模块热替换 */
     hot: true,
